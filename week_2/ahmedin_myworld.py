075e842da6530dff7515c5c9f32582cb92d023d2
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-03-25 15:55:42 -0400

    week-2

---
+++ week_2/ahmedin_myworld.py
@@ -0,0 +1,10 @@
+""" 
+    Title: ahmedin_myworld.py
+    Author: Professor Krasso
+    Date: 25 March 2023
+    Modified By: Yakut Ahmedin
+    Description: Assignment 2.3 Python.
+"""
+
+
+print("You are now in Ahmedin’s world!")
